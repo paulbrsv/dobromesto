446da7f91cb359e058b7221aed8cf91a50fd8423
paulbrsv <62184966+paulbrsv@users.noreply.github.com>
2025-03-15 23:28:28 +0100

    upd

--- dist/assets/feedback-legacy-CT43Vjfn.js
+++
@@ -1,1 +0,0 @@
-System.register(["./header-legacy-BT1bDr9V.js","./styles-legacy-CpuvYaH_.js","./terms-legacy-Dt5ekGkM.js"],(function(e,s){"use strict";return{setters:[null,null,null],execute:function(){}}}));
---
+++ dist/assets/viewer-legacy-CQznZz-C.js
@@ -0,0 +1,1 @@
+System.register(["./styles-legacy-BLxE1AJn.js"],(function(e,t){"use strict";return{setters:[null],execute:function(){}}}));
