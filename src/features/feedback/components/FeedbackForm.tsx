c6ad541687b8654d8fbd83b4a389ef71b71de23d
paulbrsv <62184966+paulbrsv@users.noreply.github.com>
2025-10-26 19:53:13 +0100

    Use local react-hook-form module

--- src/features/feedback/components/FeedbackForm.tsx
+++ src/features/feedback/components/FeedbackForm.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { styled } from 'styled-components';
-import { useForm } from '../../../react-hook-form';
+import { useForm } from 'react-hook-form';
 import { FeedbackMode, FeedbackPlaceOption } from '../types';
 
 interface FeedbackFormProps {
